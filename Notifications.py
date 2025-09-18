ddb7a38cd632c0e45bff7c4c0b7537d378678a78
PDB <24852246+PatrickBelgium@users.noreply.github.com>
2025-09-18 17:52:38 +0200

    Add files via upload

---
+++ Notifications.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from MySecrets import secrets
+from datetime import datetime
+
+def notif(title,message):
+    try:
+        apiurl="https://www.pushsafer.com/api"
+        notif_title=title
+        notif_msg=message
+        notif_icon="1"
+        notif_priority = "2"
+        notif_device=secrets.get('PUSHSAFER_DEVICE_ID') # from pushsafer account >> device ID nbr
+        notif_key=secrets.get('PUSHSAFER_PRIVATE_KEY') # from pushsafer account >> your key
+        mydata = {'t':notif_title,
+              'i':notif_icon,
+              'm':notif_msg,
+              'pr':notif_priority,
+              'd':notif_device,
+              'k':notif_key}  
+        # uncomment if sending a new notification is wanted
+        x=requests.post(apiurl,data=mydata)
+        # example binary response: b'{"status":1,"success":"message transmitted","available":921,"message_ids":"59693451:57813"}'
+
+        # uncomment following 2 lines if only historical messages info for this device are needed :
+        #mymessages = "-m?k=" + notif_key + "&d=" + notif_device
+        #x=requests.get("https://www.pushsafer.com/api" + mymessages)
+
+
+        # uncomment following 2 lines if only available notifications/calls is requested without sending a new notification
+        #myavailablecalls = "-k?k=" + notif_key + "&u=" + secrets.get('MY_EMAIL') 
+        #x=requests.get("https://www.pushsafer.com/api" + myavailablecalls)
+        # example binary response:  b'{"status":1,"success":"valid key","available-api-calls":500}' 
+
+        print(str(datetime.now().time())+ "\t" +"pushsafer response : " + str(x.content))
+
+    except Exception as error:
+        # in case your Python code has no internet access or other issues
+        print(error)
+    return
+
+notif("TEST","my message")
