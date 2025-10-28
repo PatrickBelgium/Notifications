5d98cfa5a260c70c3c83969c625f9b6c289a1321
PatrickBelgium <patrick_debruyne@hotmail.com>
2025-10-28 14:16:14 +0100

    my secrets 2

---
+++ MySecrets.py
@@ -0,0 +1,7 @@
+# dictionary of personal credentials ( like pushsafer , email , paswords , ....)
+secrets = {
+    'PUSHSAFER_PRIVATE_KEY': "abcd1234efghijkl567",
+    'PUSHSAFER_DEVICE_ID': "12345",
+    'MY_EMAIL': 'my_email@gmail.com',
+
+}
