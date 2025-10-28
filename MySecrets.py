3eb6bfdbccc239bde2b68fc7be1f9cfe5c7b4136
PatrickBelgium <patrick_debruyne@hotmail.com>
2025-10-28 14:56:04 +0100

    gitignore fix

--- MySecrets.py
+++
@@ -1,7 +0,0 @@
-# dictionary of personal credentials ( like pushsafer , email , paswords , ....)
-secrets = {
-    'PUSHSAFER_PRIVATE_KEY': "abcd1234efghijkl567",
-    'PUSHSAFER_DEVICE_ID': "12345",
-    'MY_EMAIL': 'myemail@gmail.com',
-
-}
